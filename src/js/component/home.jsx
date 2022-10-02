2985205f2a04cc1c0922f4b7001db72e07427332
Iván Carpio <ivancarpio50@gmail.com>
2022-10-02 06:39:15 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [lights, setLights] = useState(undefined)
+	const [addColor, setAddColor] = useState(undefined)
+
+	const handleOn = (isOn) =>{
+		if(lights === isOn){
+			setLights(undefined)
+		}
+		else{
+			setLights(isOn)
+		}
+	}
+
+	const handleChange = () =>{
+		let colors = ["red","yellow","green","purple"]
+		let i = 0;
+		setInterval(()=>{
+			if(addColor){
+				setLights(colors[i%4])
+			}
+			else{
+				setLights(colors[i%3])
+			}
+			i++
+		},1000)
+
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="stick"></div>
+			<div className={addColor ? "base isPurpleOn": "base"}>
+					<div className={lights == "red" ? "light red glow" : "light red"} onClick={() => {handleOn("red")}}></div>
+					<div className={lights == "yellow" ? "light yellow glow" : "light yellow"} onClick={() => {handleOn("yellow")}}></div>
+					<div className={lights == "green" ? "light green glow" : "light green"} onClick={() => {handleOn("green")}}></div>
+					{addColor && <div className={lights == "purple" ? "light purple glow" : "light purple"} onClick={() => {handleOn("purple")}}></div>}
+			</div>
+			<button className="btn" onClick={()=>{handleChange()}}>Change</button>
+			<button className="btn" onClick={()=>{setAddColor(!addColor)}}>Add Purple</button>
 		</div>
 	);
 };
